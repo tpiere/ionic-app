faca3c1d5ede958137d9c9e96c9d5aa3c5be52ae
Tom Piere <tpiere@gmail.com>
2017-02-15 19:03:06 -0600

    changed to use root www for deployment

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var port = normalizePort(process.env.PORT || '8100');
 app.set('port', port);
 
 app.get('/', function (request, response) {
-    response.sendFile(__dirname + "/platforms/browser/www/index.html");
+    response.sendFile(__dirname + "/www/index.html");
 });
 
-app.use(express.static(__dirname + '/platforms/browser/www'));
+app.use(express.static(__dirname + '/www'));
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port );
